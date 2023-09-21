ceda0e621f68595529fe65058a9121e2ef574e08
Tanveer Hussain <thussain@cinnova.com>
2023-09-21 19:19:49 +0500

    | Changes | TH |

--- AutoIntell.py
+++ AutoIntell.py
@@ -5,7 +5,51 @@ from PyQt5.QtCore import Qt, QTimer, QObject, pyqtSignal, QThread
 from PyQt5.QtGui import QImage, QPixmap, QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QFileDialog
 
+def detect_objects(image, threshold=0.5):
+    cfg_path = 'yolo/yolov3.cfg'
+    weight_path = 'yolo/yolov3.weights'
+    coco_names_path = 'yolo/coco.names'
+    output = np.copy(image)
+    
+    # Load YOLOv3 weights and configuration
+    net = cv2.dnn.readNetFromDarknet(cfg_path, weight_path)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+
+    # Load COCO class labels (you can customize this for your specific use case)
+    classes = open(coco_names_path).read().strip().split('\n')
+
+    # Get height and width of the output
+    height, width, _ = output.shape
+
+    # Create a blob from the output and pass it through the network
+    blob = cv2.dnn.blobFromImage(output, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layer_names = net.getUnconnectedOutLayersNames()
+    detections = net.forward(layer_names)
+
+    # Loop over the detections and draw rectangles around objects
+    for detection in detections:
+        for obj in detection:
+            scores = obj[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > threshold:  # Adjust the confidence threshold as needed
+                center_x = int(obj[0] * width)
+                center_y = int(obj[1] * height)
+                obj_width = int(obj[2] * width)
+                obj_height = int(obj[3] * height)
+                x = int(center_x - obj_width / 2)
+                y = int(center_y - obj_height / 2)
+
+                # Draw a rectangle around the detected object
+                cv2.rectangle(output, (x, y), (x + obj_width, y + obj_height), (0, 255, 0), 2)
+                label = f"{classes[class_id]}: {confidence:.2f}"
+                cv2.putText(output, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    return output
+
 class Worker(QObject):
+
     image_signal = pyqtSignal(np.ndarray)
 
     def __init__(self):
@@ -15,7 +59,12 @@ class Worker(QObject):
         image = cv2.imread(file_name)
         self.image_signal.emit(image)
 
+    def detect_objects(self, frame):
+        frame = detect_objects(frame, threshold=0.5)
+        self.image_signal.emit(frame)
+
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
@@ -118,10 +167,22 @@ class MainWindow(QMainWindow):
         if ret:
             self.frame = frame
             if not self.is_capturing:
+                # Detect objects and draw rectangles on the frame
+                self.worker.detect_objects(frame)
+            else:
                 self.update_image_label(frame)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+
+    # image = cv2.imread('horse.jpg')
+    # print('[image]', image.shape)
+    # cv2.imshow('input', image)
+    # image = detect_objects(image, threshold=0.9)
+    # print('[image]', image.shape)
+    # cv2.imshow('output', image)
+    # cv2.waitKey(0)
+
     sys.exit(app.exec_())
